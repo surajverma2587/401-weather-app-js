9cfdfd9fd543fe9da1f3b5351ea1ceaebfb0a0b2
surajverma2587 <surajverma2587@gmail.com>
2023-09-26 20:05:17 +0100

    handle form validation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,41 @@
-console.log("Hello from JS");
+const searchForm = document.getElementById("searchForm");
+
+const renderErrorMessage = () => {
+  // target parent
+  const searchInputContainer = document.getElementById("searchInputContainer");
+
+  // construct error message element
+  const errorMessage = document.createElement("div");
+  errorMessage.setAttribute("class", "form-text text-danger");
+  errorMessage.setAttribute("id", "errorMessage");
+  errorMessage.textContent = "Please enter a city name.";
+
+  // append error message element
+  searchInputContainer.appendChild(errorMessage);
+};
+
+const clearErrorMessage = () => {
+  const errorMessage = document.getElementById("errorMessage");
+
+  if (errorMessage) {
+    errorMessage.remove();
+  }
+};
+
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+
+  // get input value
+  const searchInput = document.getElementById("searchInput");
+  const cityName = searchInput.value;
+
+  // validation
+  if (!cityName) {
+    // render form error message
+    renderErrorMessage();
+  } else {
+    clearErrorMessage();
+  }
+};
+
+searchForm.addEventListener("submit", handleFormSubmit);
