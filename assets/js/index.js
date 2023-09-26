8df1f42a786ea95ef88b9d3feae5e276fcfe6fe5
surajverma2587 <surajverma2587@gmail.com>
2023-09-26 20:32:29 +0100

    connect to API and render components

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,6 +22,77 @@ const clearErrorMessage = () => {
   }
 };
 
+const renderErrorAlert = () => {
+  // target parent
+  const weatherInfo = document.getElementById("weatherInfo");
+
+  weatherInfo.innerHTML = `<div class="alert alert-info" role="alert">No weather info found!</div>`;
+};
+
+const getWeatherInfoByCity = async (cityName) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=8109f605d79877f7488a194794a29013`;
+
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  }
+};
+
+const constructWeatherCard = (weatherData) => {
+  // target parent
+  const weatherInfo = document.getElementById("weatherInfo");
+
+  // construct weather card
+  const weatherCard = `<div class="card">
+    <div class="card-body">
+      <h5 class="card-title">${weatherData.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${weatherData.weather[0].description.toUpperCase()}</h6>
+      <ul class="list-group">
+        <li class="list-group-item">
+          <div class="d-flex justify-content-between">
+            <div>Temperature</div>
+            <div>${weatherData?.main?.temp}</div>
+          </div>
+        </li>
+        <li class="list-group-item">
+          <div class="d-flex justify-content-between">
+            <div>Pressure</div>
+            <div>${weatherData.main.pressure}</div>
+          </div>
+        </li>
+        <li class="list-group-item">
+          <div class="d-flex justify-content-between">
+            <div>Wind Speed</div>
+            <div>${weatherData.wind.speed}</div>
+          </div>
+        </li>
+        <li class="list-group-item">
+          <div class="d-flex justify-content-between">
+            <div>Humidity</div>
+            <div>${weatherData.main.humidity}</div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>`;
+
+  // set parent HTML with weather card
+  weatherInfo.innerHTML = weatherCard;
+};
+
+const renderWeatherInfo = async (cityName) => {
+  // get data from API for cityName
+  const weatherData = await getWeatherInfoByCity(cityName);
+
+  if (weatherData) {
+    constructWeatherCard(weatherData);
+  } else {
+    renderErrorAlert();
+  }
+};
+
 const handleFormSubmit = (event) => {
   event.preventDefault();
 
@@ -35,6 +106,8 @@ const handleFormSubmit = (event) => {
     renderErrorMessage();
   } else {
     clearErrorMessage();
+
+    renderWeatherInfo(cityName);
   }
 };
 
